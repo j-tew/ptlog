b60ce89828695e9fcdcc76fd1481fc1b9e07720d
j-tew <justinrtew@gmail.com>
2024-03-12 00:51:15 -0500

    Cookin' with gasoline now
    Converted from CDN to local pico and htmx, and also trying a different
    color variant of pico. *Fun fact: Go needs a handler for static files*

--- main.go
+++ main.go
@@ -9,12 +9,15 @@ import (
 )
 
 func main() {
+    fs := http.FileServer(http.Dir("web/static"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
 
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         tmpl := template.Must(template.ParseFiles("web/index.html"))
         tmpl.Execute(w, nil)
     })
-    http.HandleFunc("/clicked", func(w http.ResponseWriter, r *http.Request) {
+
+    http.HandleFunc("GET /clicked", func(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Clicked!")
     })
 

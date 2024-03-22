b5a885f44ea852985711f3879864a353e97ca491
j-tew <justinrtew@gmail.com>
2024-03-21 21:41:56 -0500

    Consolidated and refactored
    Consolidated to one file, since it doesn't add value to split out yet.
    
    I also changed up the DB logic with a model and workout structs.
    
    Had to split the date out into ints. Will figure out how to handle date
    later.
    
    Haven't gotten to rendering the data in the template yet.

--- main.go
+++ main.go
@@ -4,27 +4,38 @@ import (
 	"fmt"
 	"io"
 	"log"
+    "time"
 	"net/http"
 	"html/template"
     "database/sql"
 
-	"github.com/j-tew/ptlog/internal/workout"
-
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func main() {
+type model struct {
+    DB *sql.DB
+    workout workout
+}
+
+type workout struct {
+    name string
+    day, month, year int
+    duration int
+}
+
+func (m *model) setup() error {
     db, err := sql.Open("sqlite3", "./ptlog.db")
     if err != nil {
-        log.Fatal(err)
+        return err
     }
-
-    defer db.Close()
 
     stmt := `
     create table if not exists workouts(
         id integer primary key autoincrement,
-        date datetime, name text,
+        name text,
+        day integer,
+        month integer,
+        year integer,
         duration integer
     );
     delete from workouts;
@@ -32,11 +43,84 @@ func main() {
     _, err = db.Exec(stmt)
     if err != nil {
         log.Printf("%q: %s\n", err, stmt)
-        return
+        return err
+    }
+    m.DB = db
+    return nil
+}
+
+func (m *model) addWorkout(w workout) error {
+    db := m.DB
+    tx, err := db.Begin()
+    if err != nil {
+        return err
     }
 
-    w := workout.Workout{Name: "run", Duration: 30}
-    workout.Add(db, w)
+    stmt, err := tx.Prepare("insert into workouts(name, day, month, year, duration) values(?, ?, ?, ?, ?)")
+    if err != nil {
+        return err
+    }
+
+    defer stmt.Close()
+
+    _, err = stmt.Exec(w.name, w.day, w.month, w.year, w.duration)
+    if err != nil {
+        return err
+    }
+
+    err = tx.Commit()
+    if err != nil {
+        return err
+    }
+    return nil
+}
+
+func (m *model) getWorkouts() error {
+    db := m.DB
+    rows, err := db.Query("select name, day, month, year, duration from workouts")
+    if err != nil {
+        return err
+    }
+    defer rows.Close()
+
+    var workouts []workout
+    for rows.Next() {
+        var w workout
+        err = rows.Scan(&w.name, &w.day, &w.month, &w.year, &w.duration)
+        if err != nil {
+            return err
+        }
+        workouts = append(workouts, w)
+    }
+    err = rows.Err()
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(workouts)
+    return nil
+}
+
+func main() {
+    m := model{}
+
+    err := m.setup()
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer m.DB.Close()
+
+    year, month, day := time.Now().Date()
+    w := workout{name: "run", day: day, month: int(month), year: year, duration: 30}
+
+    err = m.addWorkout(w)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = m.getWorkouts()
+    if err != nil {
+        log.Fatal(err)
+    }
 
     fs := http.FileServer(http.Dir("web/static"))
     http.Handle("/static/", http.StripPrefix("/static/", fs))
